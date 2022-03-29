72aae732d4c222152533417dd4d09e1d427ee579
Dilekcn <canturkdk@gmail.com>
2022-03-29 22:31:48 +0100

    array

--- 6kyu/a.js
+++ 6kyu/a.js
@@ -9,12 +9,3 @@ for(let i=0; i<countries.length; i++){
 }
 console.log(resultArray);
 const countries= ['Finland','Denmark','Sweden','Norway','Iceland']
-let resultArray=[];
-for(let i=0; i<countries.length; i++){
-    let first=countries[i].toUpperCase();
-    let second=first.slice(0,3);
-    let third=countries[i].length;
-    let temp=[first, second, third];
-    resultArray.push(temp);
-}
-console.log(resultArray);
